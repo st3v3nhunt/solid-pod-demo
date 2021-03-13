387c0863158799212e7cc220fe2802c2b67969b9
Steven Hunt <st3v3nhunt@users.noreply.github.com>
2021-03-13 18:11:22 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# starter-node-repo
+
+[![JavaScript Style Guide](https://img.shields.io/badge/code_style-standard-brightgreen.svg)](https://standardjs.com)
+[![Known Vulnerabilities](https://snyk.io/test/github/st3v3nhunt/starter-node-repo/badge.svg)](https://snyk.io/test/github/st3v3nhunt/starter-node-repo)
+
+> A blank repo with some useful modules installed to save time when starting a
+> new project
+
+## Usage
+
+Create a new repository from this template and run `./rename.js` specifying the
+new name of the repository and the description to use e.g.
+
+```shell
+./rename.js my-new-repo "Really useful description"
+```
+
+Once the rename has been completed the script can be removed.
+
+If there is an issue during the rename the changes can be reverted via
+`git clean -df && git checkout -- .`
+
+## Running the application
+
+The application is Dockerised and can be run via `docker` or `docker compose`
+commands. The easiest option is to use the `docker-compose` files and run the
+application via `docker-compose up --build`. Alternatively, the same can be
+achieved by building the image and running it with docker:
+
+```shell
+docker build . --build-arg NODE_ENV=development -t starter-node-repo
+
+docker run --init -p 3000:3000 -p 9229:9229 -t starter-node-repo
+```
---
+++ rename.js
@@ -0,0 +1,106 @@
+#!/usr/bin/env node
+
+const fs = require('fs')
+const readline = require('readline')
+
+const { description: originalDescription, name: originalProjectName } = require('./package.json')
+
+function processInput (args) {
+  const [, , projectName, description] = args
+  if (args.length === 2) {
+    console.error('Please enter a new name and description for the project e.g. my-new-repo "Really useful description".')
+    process.exit(1)
+  }
+  if (args.length !== 4 || !projectName || !description) {
+    const errMsg = [
+      'Please enter a new name and description for the project.',
+      'The description must not be empty and be wrapped in quotation marks e.g. "Really useful description".'
+    ]
+    console.error(errMsg.join('\n'))
+    process.exit(1)
+  }
+  return { description, projectName }
+}
+
+async function confirmRename (projectName, description) {
+  const affirmativeAnswer = 'yes'
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  })
+  return new Promise((resolve, reject) => {
+    rl.question(`Do you want to rename the project to '${projectName}', with a description of '${description}'?\nType '${affirmativeAnswer}' to confirm\n`, (answer) => {
+      rl.close()
+      resolve(answer === affirmativeAnswer)
+    })
+  })
+}
+
+function getScriptDir () {
+  return './scripts'
+}
+
+// Not all of these files are in the repo, the rename will ignore the error if
+// any are missing and these might be useful to add at some point
+function getRootFiles () {
+  return ['docker-compose.yaml', 'package.json', 'package-lock.json']
+}
+
+function getScriptFiles () {
+  const scriptDir = getScriptDir()
+  const files = ['test']
+  return files.map((file) => {
+    return `${scriptDir}/${file}`
+  })
+}
+
+async function updateProjectName (projectName) {
+  const rootFiles = getRootFiles()
+  const scriptFiles = getScriptFiles()
+  const filesToUpdate = [...rootFiles, ...scriptFiles]
+
+  console.log(`Updating projectName from '${originalProjectName}', to '${projectName}'. In...`)
+  await Promise.all(filesToUpdate.map(async (file) => {
+    console.log(file)
+    try {
+      const content = await fs.promises.readFile(file, 'utf8')
+      const projectNameRegex = new RegExp(originalProjectName, 'g')
+      const updatedContent = content.replace(projectNameRegex, projectName)
+      return fs.promises.writeFile(file, updatedContent)
+    } catch (err) {
+      console.error(err)
+    }
+  }))
+  console.log('Completed projectName update.')
+}
+
+async function updateProjectDescription (description) {
+  const filesToUpdate = ['package.json']
+
+  console.log(`Updating description from '${originalDescription}', to '${description}'. In...`)
+  await Promise.all(filesToUpdate.map(async (file) => {
+    console.log(file)
+    const content = await fs.promises.readFile(file, 'utf8')
+    const updatedContent = content.replace(originalDescription, description)
+    return fs.promises.writeFile(file, updatedContent)
+  }))
+  console.log('Completed description update.')
+}
+
+async function rename () {
+  const { description, projectName } = processInput(process.argv)
+  const rename = await confirmRename(projectName, description)
+  if (rename) {
+    console.time('rename')
+    Promise
+      .allSettled([
+        updateProjectName(projectName),
+        updateProjectDescription(description)
+      ])
+      .then(() => console.timeEnd('rename'))
+  } else {
+    console.log('Project has not been renamed.')
+  }
+}
+
+rename()
